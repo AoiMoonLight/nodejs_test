0ea739645594bb89618f7b15e2436734f71db7a7
AoiMoonLight <aoi.moonlight@gmail.com>
2018-04-02 19:51:20 +0900

    test server

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+var http = require ('http');
+
+http.createServer (function (request, response) {
+  response.writeHead (200,{'Content-Type': 'text/html'});
+  response.end('<h1>Welcome to node.js</h1>');
+
+}).listen (52222, function() {
+  console.log('Listening on 127.0.0.1:52222');
+});
