dc618e744d4488da3c9eaf5f273c5c2514037b0d
AoiMoonLight <aoi.moonlight@gmail.com>
2018-05-30 11:27:02 +0900

    add principal api

--- wsServer.js
+++ wsServer.js
@@ -22,6 +22,7 @@ var currency = ['BTC', 'ETH', 'DASH', 'LTC', 'ETC', 'XRP', 'BCH', 'XMR', 'ZEC', 
                 'ZIL', 'ETHOS', 'PAY', 'WAX', 'POWR', 'LRC', 'GTO', 'STEEM', 'STRAT'];
 var period = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '1d', '1w'];
 var schema = require('./DB_Schema');
+var cycle_count = 0;
 
 
 var redis = require('redis'),
@@ -33,6 +34,7 @@ function noop () {}
 function heartbeat() {
   this.isAlive = true;
 }
+
 
 wss.on('connection', function connection(ws, req) {
   ws.isAlive = true;
@@ -56,14 +58,14 @@ wss.on('connection', function connection(ws, req) {
           jwt.verify(msg.key,token_key, function(err, decoded) {
             // console.log(decoded);
             if(decoded.exp < Date.now()/1000) { //expired
-              ws.send('{"key":"expired"}');
+              ws.send('{"keystat":"expired"}');
               ws.authorized = false;
             }
             else if (decoded.exp < (Date.now()/1000)-300){ //almost expired
-              ws.send('{"key":"almost_expired"}');
+              ws.send('{"keystat":"almost_expired"}');
             }
             else {
-              response.key = "ok";
+              response.keystat = "ok";
             }
           });
           if(msg.request) {
@@ -72,11 +74,33 @@ wss.on('connection', function connection(ws, req) {
                 response.current_price = currentprice;
                 ws.send(JSON.stringify(response));
                 break;
+              case "principalUpdate" :
+                var temp_wallet = {
+                  currency : msg.currency,
+                  principal : msg.principal
+                };
+                db.update_principal(ws.iduser, temp_wallet, function(result) {
+                  console.log("principal update complete");
+                });
+                db.get_wallet(ws.iduser, function(result) {
+                  if(result.status == 0) {
+                    ws.Bithumb_wallet = result.wallet;
+                    ws.send('{"Bithumb_wallet":'+JSON.stringify(ws.Bithumb_wallet)+'}');
+                    console.log('Get user wallet data complete');
+                  }
+                });
+                break;
             }
           }
           else {
             ws.send(JSON.stringify(response));
           }
+        }
+        else {
+          ws.send('{"keystat":"wrongkey"}');
+          ws.authorized = false;
+          ws.close();
+          console.log("Wrong key");
         }
       }
       else {
@@ -90,10 +114,19 @@ wss.on('connection', function connection(ws, req) {
                 console.log("key : "+token);
                 ws.token = token;
                 ws.iduser = result.idUser;
-                ws.api = result.api;
-                ws.sapi = result.sapi;
-              })
+                ws.api = {};
+                ws.api.key = result.api;
+                ws.api.secret = result.sapi;
+                bithumb.wallet(ws.api, function(res) {
+                  if(res.status == "0000"){
+                    db.set_Wallet(ws.iduser,res.data, function(result) {
+                      console.log("wallet update complete");
+                    });
+                  };
+                });
+              });
               ws.authorized = true;
+
             }
             else {
               ws.authorized = false;
@@ -103,6 +136,7 @@ wss.on('connection', function connection(ws, req) {
         else {
           ws.send('{"error":"key_not_exist"}');
           ws.close();
+          console.log("key not exist");
         }
       }
   });
@@ -133,26 +167,31 @@ setInterval(function ping() { // user alive check
   });
 }, 30000);
 
-setInterval(() => { // Run Bithumb API in 150msec cycle each, 5000msec cycle total
-  bithumb.ticker(function(result) {
-    if(result) if(result.status == "0000") {
-      currency.forEach(function(item) {
-        var temp ={
-          price:'',
-          fluctate:'',
-          rate:''
-        };
-        currentprice[item] = temp;
-        currentprice[item].price = String(result.data[item].closing_price);
-        currentprice[item].fluctate = String(result.data[item]['24H_fluctate']);
-        currentprice[item].rate = String(result.data[item]['24H_fluctate_rate']);
-      });
-      var stamp = Date(result.data.date).toString();
+setInterval(() => { // Run Bithumb API in 125msec cycle each, 5000msec cycle total
+  if(++cycle_count == 40) {cycle_count = 0};
+  if(cycle_count <= 31) {
 
-      currentprice["date"] = stamp.substr(0,stamp.length - 15);
-    }
-  });
-}, 5000);
+  } else if (cycle_count == 39) {
+    bithumb.ticker(function(result) {
+      if(result) if(result.status == "0000") {
+        currency.forEach(function(item) {
+          var temp ={
+            price:'',
+            fluctate:'',
+            rate:''
+          };
+          currentprice[item] = temp;
+          currentprice[item].price = String(result.data[item].closing_price);
+          currentprice[item].fluctate = String(result.data[item]['24H_fluctate']);
+          currentprice[item].rate = String(result.data[item]['24H_fluctate_rate']);
+        });
+        var stamp = Date(result.data.date).toString();
+
+        currentprice["date"] = stamp.substr(0,stamp.length - 15);
+      }
+    });
+  }
+}, 125);
 
 
 function replacer(key, value) {
